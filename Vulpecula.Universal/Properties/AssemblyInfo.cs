8ca7877f44e80a1ba6daabaf6e2e9c0e541c628e
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2015-10-04 06:54:50 +0900

    Add icons. Ref #1

--- Vulpecula.Universal/Properties/AssemblyInfo.cs
+++ Vulpecula.Universal/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Vulpecula.Universal")]
+[assembly: AssemblyTitle("Vulpecula for Windows")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Vulpecula.Universal")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyProduct("Vulpecula for Windows")]
+[assembly: AssemblyCopyright("Copyright © Mikazuki 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
