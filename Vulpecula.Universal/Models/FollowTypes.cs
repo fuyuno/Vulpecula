634e585136e82b2a3889a4b5d4eac8fc77805e3e
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2015-11-06 01:48:25 +0900

    [Universal]ユーザーレビュー実装関連

---
+++ Vulpecula.Universal/Models/FollowTypes.cs
@@ -0,0 +1,40 @@
+﻿namespace Vulpecula.Universal.Models
+{
+    public enum FollowTypes
+    {
+        /// <summary>
+        /// フォロー中
+        /// </summary>
+        Following,
+
+        /// <summary>
+        /// 未フォロー中
+        /// </summary>
+        NoFollowing,
+
+        /// <summary>
+        /// 自分
+        /// </summary>
+        Me,
+
+        /// <summary>
+        /// フォローリクエスト送信中
+        /// </summary>
+        Pending,
+
+        /// <summary>
+        /// ブロック中
+        /// </summary>
+        Blocking,
+
+        /// <summary>
+        /// 取得できず
+        /// </summary>
+        Unknown,
+
+        /// <summary>
+        /// 取得中を示します。
+        /// </summary>
+        Loading
+    }
+}
---
+++ Vulpecula.Universal/ViewModels/Flyouts/UserFlyoutViewModel.cs
@@ -0,0 +1,91 @@
+﻿using System.Linq;
+
+using Vulpecula.Models;
+using Vulpecula.Universal.Models;
+using Vulpecula.Universal.ViewModels.Primitives;
+
+// ReSharper disable InconsistentNaming
+
+namespace Vulpecula.Universal.ViewModels.Flyouts
+{
+    public class UserFlyoutViewModel : ViewModel
+    {
+        private readonly User _user;
+
+        public UserFlyoutViewModel(User user)
+        {
+            _user = user;
+            FollowType = FollowTypes.Loading;
+
+            var me = AccountManager.Instance.Users.First();
+            AccountManager.Instance.Providers.First().Croudia.FriendShips
+                .ShowAsync(source_id => me.Id, target_id => user.Id)
+                .ContinueWith(task =>
+                {
+                    var relation = task.Result;
+                    if (AccountManager.Instance.Users.Any(w => w.Id == _user.Id))
+                    {
+                        FollowType = FollowTypes.Me;
+                        return;
+                    }
+
+                    if (relation.RelationShip.Source.IsBlocking.HasValue && relation.RelationShip.Source.IsBlocking.Value)
+                    {
+                        FollowType = FollowTypes.Blocking;
+                        return;
+                    }
+
+                    var b1 = _user.IsFollowRequestSent as bool?;
+                    if (b1 != null && b1.Value)
+                    {
+                        FollowType = FollowTypes.Pending;
+                        return;
+                    }
+
+                    var b2 = _user.IsFollowing as bool?;
+                    if (b2 == null)
+                    {
+                        FollowType = FollowTypes.Unknown;
+                        return;
+                    }
+                    FollowType = b2.Value ? FollowTypes.Following : FollowTypes.NoFollowing;
+                });
+        }
+
+        #region Properties
+
+        public string ScreenName => $"@{this._user.ScreenName}";
+
+        public string UserName => _user.Name;
+
+        public string IconUrl => _user.ProfileImageUrlHttps;
+
+        public string CoverUrl => _user.CoverImageUrlHttps;
+
+        public string Bio => _user.Description;
+
+        public string Location => _user.Location;
+
+        public long Followings => _user.FriendsCount;
+
+        public long Followers => _user.FollowersCount;
+
+        public long Favorites => _user.FavoritesCount;
+
+        public long Whispers => _user.StatusesCount;
+
+        #region FollowType
+
+        private FollowTypes _followType;
+
+        public FollowTypes FollowType
+        {
+            get { return _followType; }
+            set { this.SetProperty(ref _followType, value); }
+        }
+
+        #endregion
+
+        #endregion
+    }
+}
