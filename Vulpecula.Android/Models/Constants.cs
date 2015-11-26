8d54003ebf5c8dfd1179c888e9ba280793cb15ee
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2015-11-26 14:45:07 +0900

    [Android]Constants

---
+++ Vulpecula.Android/Models/Constants.cs
@@ -0,0 +1,17 @@
+using Vulpecula.Android.Models;
+using Vulpecula.Mobile.Models.Interfaces;
+
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof (Constants))]
+
+namespace Vulpecula.Android.Models
+{
+    public class Constants : IConstants
+    {
+        public string ConsumerKey => "542c424cd0ddacedd2b63d444c462590da8706bf4a3cd6abdc12df34451f2606";
+        public string ConsumerSecret => "14714def86ded5137d09fb335ef6e52d6e5ed3eab7de54e523b6ca2416d05313";
+        public string RedirectUrl => "https://api.mkzk.xyz";
+        public string AppKey => "xyz.mkzk.vulpecula";
+    }
+}
