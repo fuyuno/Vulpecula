1385c903f91bd50818775a618ed40abdffdccce7
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2015-12-03 18:35:36 +0900

    [Mobile]Unselectable

---
+++ Vulpecula.Mobile/Behaviors/ListViewDisableSelectionBehavior.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Vulpecula.Mobile.Behaviors
+{
+    public class ListViewDisableSelectionBehavior : Behavior<ListView>
+    {
+
+        protected override void OnAttachedTo(ListView bindable)
+        {
+            bindable.ItemSelected += Bindable_ItemSelected;
+            base.OnAttachedTo(bindable);
+        }
+
+        protected override void OnDetachingFrom(ListView bindable)
+        {
+            base.OnDetachingFrom(bindable);
+            bindable.ItemSelected -= Bindable_ItemSelected;
+        }
+
+        private void Bindable_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            ((ListView)sender).SelectedItem = null;
+        }
+
+
+    }
+}
+
