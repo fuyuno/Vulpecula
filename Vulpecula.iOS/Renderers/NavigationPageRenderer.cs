a6a6ad283741f5155f912208cf70ef3a01d7b9ee
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2015-12-09 01:57:21 +0900

    [iOS]ツールバーに関する Renderer を追加

---
+++ Vulpecula.iOS/Renderers/NavigationPageRenderer.cs
@@ -0,0 +1,45 @@
+﻿using System.Linq;
+
+using Vulpecula.iOS.Renderers;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+using System.Collections.Generic;
+using UIKit;
+
+[assembly: ExportRenderer(typeof(ContentPage), typeof(NavigationPageRenderer))]
+
+namespace Vulpecula.iOS.Renderers
+{
+    public class NavigationPageRenderer : PageRenderer
+    {
+        public NavigationPageRenderer()
+        {
+        }
+
+        public override void ViewWillAppear(bool animated)
+        {
+            base.ViewWillAppear(animated);
+
+            var bindItems = (this.Element as ContentPage)?.ToolbarItems.OrderBy(w => w.Priority);
+            var items = this.NavigationController.TopViewController.NavigationItem;
+            var rightItems = new List<UIBarButtonItem>();
+            var leftItems = items.LeftBarButtonItems?.ToList() ?? new List<UIBarButtonItem>();
+            foreach (var item in bindItems)
+            {
+                if (item.Priority < 0)
+                {
+                    leftItems.Add(item.ToUIBarButtonItem());
+                }
+                else
+                {
+                    rightItems.Add(item.ToUIBarButtonItem());
+                }
+            }
+
+            items.SetRightBarButtonItems(rightItems.ToArray(), animated);
+            items.SetLeftBarButtonItems(leftItems.ToArray(), animated);
+        }
+    }
+}
+
