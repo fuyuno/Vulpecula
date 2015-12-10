a546810454a7a3f6359b4d47ce29b907ba1068c7
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2015-12-10 13:11:46 +0900

    [Mobile]Cancel ボタンなどを追加

--- Vulpecula.Mobile/ViewModels/Popups/StatusPageViewModel.cs
+++ Vulpecula.Mobile/ViewModels/Popups/StatusPageViewModel.cs
@@ -1,25 +1,51 @@
-﻿using Prism.Navigation;
+﻿using System.Linq;
+using System.Windows.Input;
 
+using Prism.Navigation;
+
+using Vulpecula.Mobile.Models;
 using Vulpecula.Mobile.Models.Interfaces;
 using Vulpecula.Mobile.ViewModels.Primitives;
+
+using Xamarin.Forms;
+using Vulpecula.Models;
 
 namespace Vulpecula.Mobile.ViewModels.Popups
 {
     public class StatusPageViewModel : NavigationalViewModel
     {
-        public StatusPageViewModel(ILocalization localization, INavigationService navigationService) : base(localization, navigationService)
+        private readonly AccountManager _accountManager;
+
+        public StatusPageViewModel(ILocalization localization, INavigationService navigationService, AccountManager accountManager)
+            : base(localization, navigationService)
         {
+            this._accountManager = accountManager;
             NavigationTitle = "StatusPage";
+            this.MainText = "Hello!";
         }
 
-        /// <summary>
-        /// Called when the implementer has been navigated to.
-        /// </summary>
-        /// <param name="parameters">The navigation parameters.</param>
+        public override void OnNavigatedFrom(NavigationParameters parameters)
+        {
+            if (!string.IsNullOrWhiteSpace(this.Text))
+            {
+                parameters["draft"] = this.Text;
+            }
+        }
+
         public override void OnNavigatedTo(NavigationParameters parameters)
         {
-            MainText = "Hello !";
+            if (parameters == null)
+            {
+                return;
+            }
+            if (parameters.ContainsKey("draft"))
+            {
+                this.Text = parameters["draft"].ToString();
+                parameters.Remove("draft");
+            }
         }
+
+        #region Properties
 
         #region MainText
 
@@ -30,6 +56,80 @@ namespace Vulpecula.Mobile.ViewModels.Popups
             get { return _mainText; }
             set { this.SetProperty(ref _mainText, value); }
         }
+
+        #endregion
+
+        #region Text
+
+        private string _text;
+
+        public string Text
+        {
+            get { return this._text; }
+            set
+            {
+                if (this.SetProperty(ref this._text, value))
+                {
+                    this.SendCommand.ChangeCanExecute(); 
+                }
+            }
+        }
+
+        #endregion
+
+        #endregion
+
+        #region Commands
+
+
+        #region CloseCommand
+
+        private ICommand _closeCommand;
+
+        public ICommand CloseCommand
+        {
+            get
+            {
+                return this._closeCommand ?? (this._closeCommand = new Command(Close));
+            }
+        }
+
+        private void Close()
+        {
+            this.NavigationService.GoBack();
+        }
+
+        #endregion
+
+        #region SendCommand
+
+        private Command _sendCommand;
+
+        public Command SendCommand
+        {
+            get
+            {
+                return this._sendCommand ?? (this._sendCommand = new Command(Send, CanSend));
+            }
+        }
+
+        private async void Send()
+        {
+            await this._accountManager.Providers.First().Croudia.Statuses.UpdateAsync(text => this.Text);
+            this.Text = string.Empty;
+            this.NavigationService.GoBack();
+        }
+
+        private bool CanSend()
+        {
+            if (string.IsNullOrWhiteSpace(this.Text) || this.Text.Length > 372)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        #endregion
 
         #endregion
     }
