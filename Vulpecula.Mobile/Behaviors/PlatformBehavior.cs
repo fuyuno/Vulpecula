12532671948e65537d795de1810593bde4a3487f
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2015-12-15 01:56:20 +0900

    [Mobile]Add component

---
+++ Vulpecula.Mobile/Behaviors/PlatformBehavior.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Reactive.Linq;
+
+using Vulpecula.Mobile.Extensions;
+
+using Xamarin.Forms;
+
+namespace Vulpecula.Mobile.Behaviors
+{
+    /// <summary>
+    /// Platform behavior.
+    /// </summary>
+    public class PlatformBehavior : Behavior<Layout<View>>
+    {
+        private Layout<View> _layout;
+        private IDisposable _disposable;
+        private bool _flag;
+
+        protected override void OnAttachedTo(Layout<View> bindable)
+        {
+            this._layout = bindable;
+            this._disposable = Observable.Timer(TimeSpan.Zero, TimeSpan.FromMilliseconds(10))
+                .Select(_ => this._flag)
+                .DistinctUntilChanged()
+                .Where(w => w)
+                .Repeat()
+                .Subscribe(_ => this.DoAction());
+            bindable.ChildAdded += Bindable_ChildAdded;
+            bindable.ChildRemoved += Bindable_ChildRemoved;
+
+            base.OnAttachedTo(bindable);
+        }
+
+        protected override void OnDetachingFrom(Layout<View> bindable)
+        {
+            base.OnDetachingFrom(bindable);
+
+            bindable.ChildAdded -= Bindable_ChildAdded;
+            bindable.ChildRemoved -= Bindable_ChildRemoved;
+            this._disposable.Dispose();
+        }
+
+        private void Bindable_ChildRemoved(object sender, ElementEventArgs e)
+        {
+            //
+        }
+
+        private void Bindable_ChildAdded(object sender, ElementEventArgs e)
+        {
+            this._flag = true;
+        }
+
+        private void DoAction()
+        {
+            var children = this._layout.Children;
+            foreach (var child in children)
+            {
+                if (Device.OS == TargetPlatform.Android)
+                {
+                    if (!(bool)child.GetValue(PlatformControl.AndroidProperty))
+                    {
+                        this._layout.Children.Remove(child);
+                    }
+                }
+                if (Device.OS == TargetPlatform.iOS)
+                {
+                    if (!(bool)child.GetValue(PlatformControl.iOSProperty))
+                    {
+                        this._layout.Children.Remove(child);
+                    }
+                }
+            }
+
+            this._flag = false;
+        }
+
+    }
+}
+
---
+++ Vulpecula.Mobile/Extensions/PlatformControl.cs
@@ -0,0 +1,40 @@
+﻿using Xamarin.Forms;
+
+namespace Vulpecula.Mobile.Extensions
+{
+    public class PlatformControl
+    {
+        // iOS
+
+        public static bool GetiOS(BindableObject obj)
+        {
+            return (bool)obj.GetValue(iOSProperty);
+        }
+
+        public static void SetiOS(BindableObject obj, bool value)
+        {
+            obj.SetValue(iOSProperty, value);
+        }
+
+        public static readonly BindableProperty iOSProperty = 
+            BindableProperty.CreateAttached("iOS", typeof(bool), typeof(PlatformControl), true);
+
+
+        // Android
+
+        public static bool GetAndroid(BindableObject obj)
+        {
+            return (bool)obj.GetValue(AndroidProperty);
+        }
+
+        public static void SetAndroid(BindableObject obj, bool value)
+        {
+            obj.SetValue(AndroidProperty, value);
+        }
+
+        public static readonly BindableProperty AndroidProperty =
+            BindableProperty.CreateAttached("Android", typeof(bool), typeof(PlatformControl), true);
+
+    }
+}
+
