78f2e18cebbde118583dd022318a9e1b3de69825
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2015-12-17 00:03:09 +0900

    [iOS]アイテムがない場所の不思議な空間を削除

---
+++ Vulpecula.iOS/Renderers/ListViewControlRenderer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Xamarin.Forms;
+using Vulpecula.iOS.Renderers;
+using Xamarin.Forms.Platform.iOS;
+using UIKit;
+
+[assembly: ExportRenderer(typeof(ListView), typeof(ListViewControlRenderer))]
+
+namespace Vulpecula.iOS.Renderers
+{
+    public class ListViewControlRenderer : ListViewRenderer
+    {
+        public ListViewControlRenderer()
+        {
+            this.ElementChanged += (sender, e) =>
+            {
+                var element = this.Element as ListView;
+                if (element == null)
+                {
+                    return;
+                }
+                element.Footer = new ContentView();
+            };
+        }
+    }
+}
+
