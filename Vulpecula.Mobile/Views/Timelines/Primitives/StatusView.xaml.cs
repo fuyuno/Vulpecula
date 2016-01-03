a2e39eca0403e1d28d46afe8997075101d5b4481
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2016-01-03 18:09:41 +0900

    [Mobile]View の切り出し

---
+++ Vulpecula.Mobile/Views/Timelines/Primitives/StatusView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace Vulpecula.Mobile.Views.Timelines.Primitives
+{
+    public partial class StatusView : ContentView
+    {
+        public StatusView()
+        {
+            InitializeComponent();
+        }
+    }
+}
+
