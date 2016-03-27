256bea90c7a6497ea541e8a4a0a0732c6c6118f1
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2016-03-28 02:48:51 +0900

    [Universal]ツイートを実装

--- Vulpecula.Universal/ViewModels/Pages/TweetPageViewModel.cs
+++ Vulpecula.Universal/ViewModels/Pages/TweetPageViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.ObjectModel;
 using System.Linq;
 
+using Prism.Commands;
 using Prism.Windows.Navigation;
 
 using Vulpecula.Universal.Models;
@@ -19,6 +20,8 @@ namespace Vulpecula.Universal.ViewModels.Pages
                                      .ToList()
                                      .AsReadOnly();
             SelectedAccount = Accounts.First();
+            WhisperCount = 372;
+            WhisperText = string.Empty;
         }
 
         #region Properties
@@ -27,9 +30,10 @@ namespace Vulpecula.Universal.ViewModels.Pages
 
         #region SelectedAccount
 
-        private UserAccountViewModel _selectedAccount;
+        // TODO: object to UserAccountViewModel converter
+        private object _selectedAccount;
 
-        public UserAccountViewModel SelectedAccount
+        public object SelectedAccount
         {
             get { return _selectedAccount; }
             set { SetProperty(ref _selectedAccount, value); }
@@ -37,6 +41,63 @@ namespace Vulpecula.Universal.ViewModels.Pages
 
         #endregion SelectedAccount
 
+        #region WhisperText
+
+        private string _whisperText;
+
+        public string WhisperText
+        {
+            get { return _whisperText; }
+            set
+            {
+                if (SetProperty(ref _whisperText, value))
+                {
+                    WhisperCount = 372 - _whisperText.Length;
+                    SendTweetCommand.RaiseCanExecuteChanged();
+                }
+            }
+        }
+
+        #endregion WhisperText
+
+        #region WhisperCount
+
+        private int _whisperCount;
+
+        public int WhisperCount
+        {
+            get { return _whisperCount; }
+            set { SetProperty(ref _whisperCount, value); }
+        }
+
+        #endregion WhisperCount
+
         #endregion Properties
+
+        #region Commands
+
+        #region SendTweetCommand
+
+        private DelegateCommand _sendTweetCommand;
+
+        public DelegateCommand SendTweetCommand => _sendTweetCommand ??
+                                                   (_sendTweetCommand = new DelegateCommand(SendTweet, CanSendTweet));
+
+        private void SendTweet()
+        {
+            (SelectedAccount as UserAccountViewModel)?.SendWhisper(WhisperText);
+            _navigationService.GoBack();
+        }
+
+        private bool CanSendTweet()
+        {
+            if (WhisperText.Length == 0 || WhisperText.Length > 372)
+                return false;
+            return true;
+        }
+
+        #endregion SendTweetCommand
+
+        #endregion Commands
     }
 }
